d74e4cd1222504ba77c6e6bf4e7b42e55dca1892
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-03-19 01:01:18 +0200

    initial commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+
+function App(): JSX.Element {
+  return <SafeAreaView />;
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
