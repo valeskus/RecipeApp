12a132b72507b792acb11e131c28c7de1ccd82b9
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-03-21 12:21:17 +0200

    added category card

--- src/UI/Playground/Playground.tsx
+++ src/UI/Playground/Playground.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import {Button} from '../Button';
+import {CategoryCard} from '../CategoryCard';
 
 export const Playground: React.FC<{}> = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.PlaygroundContainer}>
       <Button icon="like" onPress={() => {}} active={true} />
       <Button icon="carbs" onPress={() => {}} />
       <Button icon="menu" onPress={() => {}} active={true} />
+      <CategoryCard title="Breacfast" onPress={() => {}} />
+      <CategoryCard title="Lunch" onPress={() => {}} />
     </SafeAreaView>
   );
 };
+const styles = StyleSheet.create({
+  PlaygroundContainer: {
+    flex: 1,
+  },
+});
