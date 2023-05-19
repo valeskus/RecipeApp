df75ae188288dc3f6b744be350b21837ab6fc7ad
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-05-19 20:55:58 +0300

    fixed categories list skeleton

--- src/screens/CategoriesList/components/CategoryListSkeleton/CategoryListSkeleton.tsx
+++ src/screens/CategoriesList/components/CategoryListSkeleton/CategoryListSkeleton.tsx
@@ -5,25 +5,24 @@ import {Colors} from '../../../../UI/Colors';
 export function CategoryListSkeleton(): JSX.Element {
   return (
     <ContentLoader
-      // viewBox="0 0 300 590"
       width={'100%'}
       height={'100%'}
       backgroundColor={Colors.secondary}
       opacity={0.2}>
-      <Rect x="10" y="1" rx="20" ry="20" width="45%" height="105" />
-      <Rect x="10" y="120" rx="4" ry="4" width="45%" height="13" />
-      <Rect x="160" y="1" rx="20" ry="20" width="45%" height="105" />
-      <Rect x="160" y="120" rx="4" ry="4" width="45%" height="13" />
+      <Rect x="3%" y="10" rx="20" ry="20" width="43%" height="135" />
+      <Rect x="3%" y="155" rx="4" ry="4" width="43%" height="13" />
+      <Rect x="53%" y="10" rx="20" ry="20" width="43%" height="135" />
+      <Rect x="53%" y="155" rx="4" ry="4" width="43%" height="13" />
 
-      <Rect x="10" y="150" rx="20" ry="20" width="45%" height="105" />
-      <Rect x="10" y="270" rx="4" ry="4" width="45%" height="13" />
-      <Rect x="160" y="150" rx="20" ry="20" width="45%" height="105" />
-      <Rect x="160" y="270" rx="4" ry="4" width="45%" height="13" />
+      <Rect x="3%" y="200" rx="20" ry="20" width="43%" height="135" />
+      <Rect x="3%" y="345" rx="4" ry="4" width="43%" height="13" />
+      <Rect x="53%" y="200" rx="20" ry="20" width="43%" height="135" />
+      <Rect x="53%" y="345" rx="4" ry="4" width="43%" height="13" />
 
-      <Rect x="10" y="300" rx="20" ry="20" width="45%" height="105" />
-      <Rect x="10" y="420" rx="4" ry="4" width="45%" height="13" />
-      <Rect x="160" y="300" rx="20" ry="20" width="45%" height="105" />
-      <Rect x="160" y="420" rx="4" ry="4" width="45%" height="13" />
+      <Rect x="3%" y="390" rx="20" ry="20" width="43%" height="135" />
+      <Rect x="3%" y="535" rx="4" ry="4" width="43%" height="13" />
+      <Rect x="53%" y="390" rx="20" ry="20" width="43%" height="135" />
+      <Rect x="53%" y="535" rx="4" ry="4" width="43%" height="13" />
     </ContentLoader>
   );
 }
