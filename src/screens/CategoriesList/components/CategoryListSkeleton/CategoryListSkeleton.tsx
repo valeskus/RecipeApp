cf1536bf395e08bfad3b98afd166b952ada8350a
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-05-19 18:24:54 +0300

    fixed skeleton width

--- src/screens/CategoriesList/components/CategoryListSkeleton/CategoryListSkeleton.tsx
+++ src/screens/CategoriesList/components/CategoryListSkeleton/CategoryListSkeleton.tsx
@@ -8,20 +8,20 @@ export function CategoryListSkeleton(): JSX.Element {
       viewBox="0 0 300 590"
       backgroundColor={Colors.secondary}
       opacity={0.2}>
-      <Rect x="10" y="1" rx="20" ry="20" width="135" height="105" />
-      <Rect x="10" y="120" rx="4" ry="4" width="135" height="13" />
-      <Rect x="160" y="1" rx="20" ry="20" width="135" height="105" />
-      <Rect x="160" y="120" rx="4" ry="4" width="135" height="13" />
+      <Rect x="10" y="1" rx="20" ry="20" width="45%" height="105" />
+      <Rect x="10" y="120" rx="4" ry="4" width="45%" height="13" />
+      <Rect x="160" y="1" rx="20" ry="20" width="45%" height="105" />
+      <Rect x="160" y="120" rx="4" ry="4" width="45%" height="13" />
 
-      <Rect x="10" y="150" rx="20" ry="20" width="135" height="105" />
-      <Rect x="10" y="270" rx="4" ry="4" width="135" height="13" />
-      <Rect x="160" y="150" rx="20" ry="20" width="135" height="105" />
-      <Rect x="160" y="270" rx="4" ry="4" width="135" height="13" />
+      <Rect x="10" y="150" rx="20" ry="20" width="45%" height="105" />
+      <Rect x="10" y="270" rx="4" ry="4" width="45%" height="13" />
+      <Rect x="160" y="150" rx="20" ry="20" width="45%" height="105" />
+      <Rect x="160" y="270" rx="4" ry="4" width="45%" height="13" />
 
-      <Rect x="10" y="300" rx="20" ry="20" width="135" height="105" />
-      <Rect x="10" y="420" rx="4" ry="4" width="135" height="13" />
-      <Rect x="160" y="300" rx="20" ry="20" width="135" height="105" />
-      <Rect x="160" y="420" rx="4" ry="4" width="135" height="13" />
+      <Rect x="10" y="300" rx="20" ry="20" width="45%" height="105" />
+      <Rect x="10" y="420" rx="4" ry="4" width="45%" height="13" />
+      <Rect x="160" y="300" rx="20" ry="20" width="45%" height="105" />
+      <Rect x="160" y="420" rx="4" ry="4" width="45%" height="13" />
     </ContentLoader>
   );
 }
--- src/screens/RecipesList/components/RecipeListSkeleton/RecipeListSkeleton.tsx
+++ src/screens/RecipesList/components/RecipeListSkeleton/RecipeListSkeleton.tsx
@@ -8,34 +8,34 @@ export function RecipeListSkeleton(): JSX.Element {
       viewBox="0 0 300 595"
       backgroundColor={Colors.secondary}
       opacity={0.2}>
-      <Circle cx="25" cy="21" r="21" />
-      <Circle cx="85" cy="21" r="21" />
-      <Circle cx="270" cy="21" r="21" />
+      <Circle cx="28" cy="21" r="21" />
+      <Circle cx="80" cy="21" r="21" />
+      <Circle cx="275" cy="21" r="21" />
 
-      <Rect x="10" y="55" rx="20" ry="20" width="135" height="105" />
-      <Rect x="17" y="170" rx="4" ry="4" width="120" height="13" />
-      <Rect x="17" y="187" rx="4" ry="4" width="100" height="15" />
-      <Rect x="17" y="205" rx="4" ry="4" width="50" height="8" />
-      <Rect x="17" y="217" rx="4" ry="4" width="50" height="8" />
-      <Rect x="158" y="55" rx="20" ry="20" width="135" height="105" />
-      <Rect x="165" y="170" rx="4" ry="4" width="120" height="13" />
-      <Rect x="165" y="187" rx="4" ry="4" width="100" height="15" />
-      <Rect x="165" y="205" rx="4" ry="4" width="50" height="8" />
-      <Rect x="165" y="217" rx="4" ry="4" width="50" height="8" />
+      <Rect x="10" y="55" rx="20" ry="20" width="45%" height="105" />
+      <Rect x="17" y="170" rx="4" ry="4" width="40%" height="13" />
+      <Rect x="17" y="187" rx="4" ry="4" width="33%" height="15" />
+      <Rect x="17" y="205" rx="4" ry="4" width="17%" height="8" />
+      <Rect x="17" y="217" rx="4" ry="4" width="17%" height="8" />
+      <Rect x="158" y="55" rx="20" ry="20" width="45%" height="105" />
+      <Rect x="165" y="170" rx="4" ry="4" width="40%" height="13" />
+      <Rect x="165" y="187" rx="4" ry="4" width="33%" height="15" />
+      <Rect x="165" y="205" rx="4" ry="4" width="17%" height="8" />
+      <Rect x="165" y="217" rx="4" ry="4" width="17%" height="8" />
 
-      <Rect x="10" y="255" rx="20" ry="20" width="135" height="105" />
-      <Rect x="17" y="370" rx="4" ry="4" width="120" height="13" />
-      <Rect x="17" y="387" rx="4" ry="4" width="100" height="15" />
-      <Rect x="17" y="405" rx="4" ry="4" width="50" height="8" />
-      <Rect x="17" y="417" rx="4" ry="4" width="50" height="8" />
-      <Rect x="158" y="255" rx="20" ry="20" width="135" height="105" />
-      <Rect x="165" y="370" rx="4" ry="4" width="120" height="13" />
-      <Rect x="165" y="387" rx="4" ry="4" width="100" height="15" />
-      <Rect x="165" y="405" rx="4" ry="4" width="50" height="8" />
-      <Rect x="165" y="417" rx="4" ry="4" width="50" height="8" />
+      <Rect x="10" y="255" rx="20" ry="20" width="45%" height="105" />
+      <Rect x="17" y="370" rx="4" ry="4" width="40%" height="13" />
+      <Rect x="17" y="387" rx="4" ry="4" width="33%" height="15" />
+      <Rect x="17" y="405" rx="4" ry="4" width="17%" height="8" />
+      <Rect x="17" y="417" rx="4" ry="4" width="17%" height="8" />
+      <Rect x="158" y="255" rx="20" ry="20" width="45%" height="105" />
+      <Rect x="165" y="370" rx="4" ry="4" width="40%" height="13" />
+      <Rect x="165" y="387" rx="4" ry="4" width="33%" height="15" />
+      <Rect x="165" y="405" rx="4" ry="4" width="17%" height="8" />
+      <Rect x="165" y="417" rx="4" ry="4" width="17%" height="8" />
 
-      <Rect x="10" y="455" rx="20" ry="20" width="135" height="105" />
-      <Rect x="158" y="455" rx="20" ry="20" width="135" height="105" />
+      <Rect x="10" y="455" rx="20" ry="20" width="45%" height="105" />
+      <Rect x="158" y="455" rx="20" ry="20" width="45%" height="105" />
     </ContentLoader>
   );
 }
