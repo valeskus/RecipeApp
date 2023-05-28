6739bce8b87c7a64c4c8924dd4df7b7a6b82fdc2
MAKARD01 <dmytro.makarenko@kingfisher.com>
2023-05-28 17:17:59 +0300

    Init search module

--- receipe-app-api/src/app.module.ts
+++ receipe-app-api/src/app.module.ts
@@ -3,9 +3,10 @@ import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {RecipeModule} from './recipe/recipe.module';
 import {CategoriesModule} from './categories/categories.module';
+import {SearchModule} from './search/search.module';
 
 @Module({
-  imports: [RecipeModule, CategoriesModule],
+  imports: [RecipeModule, CategoriesModule, SearchModule],
   controllers: [AppController],
   providers: [AppService],
 })
