34898c6481a728f847fa4010c29f4a68e929b3d8
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-06-01 16:58:03 +0300

    Add error handlers WIP

--- src/screens/CategoriesList/components/CategoryCards/useCategoryCardController.ts
+++ src/screens/CategoriesList/components/CategoryCards/useCategoryCardController.ts
@@ -1,34 +1,41 @@
-import { useNavigation } from '@react-navigation/native';
-import { useCallback, useEffect, useState } from 'react';
+import {useNavigation} from '@react-navigation/native';
+import * as CategoriesStore from '../../../../stores/categories';
+import * as SearchStore from '../../../../stores/search';
 
-import * as CategoriesStore from '@stores/categories';
-import * as SearchStore from '@stores/search';
+import {useCallback, useEffect, useState} from 'react';
 
-export const useCategoryCardController = () => {
+export const useCategoryCardControler = () => {
   const [isLoading, setLoading] = useState(false);
-  const { categories } = CategoriesStore.useCategoriesStore();
+  const {categories} = CategoriesStore.useCategoriesStore();
 
   const getCategories = CategoriesStore.useGetCategories();
 
   const navigation = useNavigation();
 
-  useEffect(() => {
+  const fetchData = async () => {
     setLoading(true);
 
-    getCategories().then(() => {
-      setLoading(false);
-    });
+    await getCategories();
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const setSearchTerm = SearchStore.useSearchTerm();
 
-  const onPress = useCallback((categoryTitle: string) => {
+  const onPress = useCallback(async (categoryTitle: string) => {
     setSearchTerm(categoryTitle);
 
     return navigation.navigate('Recipes');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return {
+    onRetry: fetchData,
     onPress,
     isLoading,
     categories,
