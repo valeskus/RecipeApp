fc06ffab2632fb580d3ff0cedb09cb42df5040df
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-09-13 17:50:00 +0300

    Add handler and states for product form

--- recipe-app-admin/src/components/ProductForm/ProductForm.tsx
+++ recipe-app-admin/src/components/ProductForm/ProductForm.tsx
@@ -6,17 +6,25 @@ import './ProductForm.style.css';
 import { useProductFormController } from './useProductFormController';
 
 export function ProductForm(): JSX.Element {
-  const { unitsValue, onSend } = useProductFormController();
+  const {  unitsValue,
+    onSend,
+    handleTitle,
+    handleUATitle,
+    handleKCal,
+    handleProteins,
+    handleCarbs,
+    handleFats,
+    handleUnits } = useProductFormController();
 
   return (
     <div className="productFormContainer">
-      <Input label="Title :" type="text" placeholder="Title" onChange={() => { }} />
-      <Input label="Title UA:" type="text" placeholder="Назва" onChange={() => { }} />
-      <Input label="kCal :" type="number" placeholder="kCal" onChange={() => { }} />
-      <Input label="Proteins :" type="number" placeholder="proteins" onChange={() => { }} />
-      <Input label="Carbs :" type="number" placeholder="carbs" onChange={() => { }} />
-      <Input label="Fats :" type="number" placeholder="fats" onChange={() => { }} />
-      <SelectComponent label="Units:" placeholder="units" multiple={false} options={unitsValue} onChange={() => { }}/>
+      <Input label="Title :" type="text" placeholder="Title" onChange={handleTitle} />
+      <Input label="Title UA:" type="text" placeholder="Назва" onChange={handleUATitle} />
+      <Input label="kCal :" type="number" placeholder="kCal" onChange={handleKCal} />
+      <Input label="Proteins :" type="number" placeholder="proteins" onChange={handleProteins} />
+      <Input label="Carbs :" type="number" placeholder="carbs" onChange={handleCarbs} />
+      <Input label="Fats :" type="number" placeholder="fats" onChange={handleFats} />
+      <SelectComponent label="Units:" placeholder="units" multiple={false} options={unitsValue} onChange={handleUnits}/>
       <Button title="Submit" onClick={onSend} />
     </div>
   );
--- recipe-app-admin/src/components/ProductForm/useProductFormController.ts
+++ recipe-app-admin/src/components/ProductForm/useProductFormController.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import * as Redux from 'react-redux';
 
 import './ProductForm.style.css';
@@ -6,41 +6,85 @@ import { ProductPostModel } from '../../stores/product/types';
 import { postProducts } from '../../stores/product/productsSlice';
 import { useGetProducts } from '../../stores/product/hooks';
 import { OptionsManager } from '../managers/OptionsManager';
+import { OptionModel } from '../common/Select/Select';
 
 export const  useProductFormController = () => {
+  const [title, setTitle] = useState<string>('');
+  const [titleUA, setTitleUA] = useState<string>('');
+  const [kCal, setKCal] = useState<number>(0);
+  const [proteins, setProteins] = useState<number>(0);
+  const [carbs, setCarbs] = useState<number>(0);
+  const [fats, setFats] = useState<number>(0);
+  const [units, setUnits] = useState<'g' | 'ml'>('g');
+
   const dispatch = Redux.useDispatch();
   const unitsValue = OptionsManager.getOptionsArray(['ml', 'g']);
 
   const getProducts = useGetProducts();
+
+  const handleTitle = useCallback((value: string) => {
+    setTitle(value);
+  }, [setTitle]);
+
+  const handleUATitle = useCallback((value: string) => {
+    setTitleUA(value);
+  }, [setTitleUA]);
+
+  const handleKCal = useCallback((value: string) => {
+    setKCal(+value);
+  }, [setKCal]);
+
+  const handleProteins = useCallback((value: string) => {
+    setProteins(+value);
+  }, [setProteins]);
+
+  const handleCarbs = useCallback((value: string) => {
+    setCarbs(+value);
+  }, [setCarbs]);
+
+  const handleFats = useCallback((value: string) => {
+    setFats(+value);
+  }, [setFats]);
+
+  const handleUnits = useCallback(({ value }: OptionModel) => {
+    if (value !== 'ml' || 'g') {
+      return;
+    }
+
+    setUnits(value);
+  }, [setUnits]);
+
   React.useEffect(() => {
     getProducts(dispatch);
   }, []);
 
-  //TODO product state
-  const product: ProductPostModel = {
-    title: 'Arugula',
-    kCal: 75,
-    proteins: 16.7,
-    carbs: 0,
-    fats: 0.8,
-    units: 'ml',
-    translations: {
-      ua: {
-        title: 'Рукола',
+  const onSend = useCallback(() => {
+    const product: ProductPostModel = {
+      title: title,
+      kCal: kCal,
+      proteins: proteins,
+      carbs: carbs,
+      fats: fats,
+      units: units,
+      translations: {
+        ua: {
+          title: titleUA,
+        },
       },
-    },
-  };
-
-  const onSend = () => {
-    if (!product) {
-      return alert('Please,check data');
-    }
+    };
 
     dispatch(postProducts(product));
-  };
+  }, [title, titleUA, kCal, proteins, carbs, fats, units]);
 
   return {
     unitsValue,
     onSend,
+    handleTitle,
+    handleUATitle,
+    handleKCal,
+    handleProteins,
+    handleCarbs,
+    handleFats,
+    handleUnits,
   };
 };
