c43693475b0f6f84327c4db0b035d108b6230fd1
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-09-13 16:12:56 +0300

    fix recipe form style

--- recipe-app-admin/src/components/RecipeForm/RecipeForm.tsx
+++ recipe-app-admin/src/components/RecipeForm/RecipeForm.tsx
@@ -94,23 +94,26 @@ export function RecipeForm(): JSX.Element {
   }, []);
 
   return (
-    <div className="productFormContainer">
-      <Input label="Title :" type="text" placeholder="Title" onChange={() => { }} />
-      <Input label="Title UA:" type="text" placeholder="Назва" onChange={() => { }} />
-      <Input label="Description:" type="text" placeholder="Description" onChange={() => { }} />
-      <Input label="Description UA:" type="text" placeholder="опис" onChange={() => { }} />
-      <SelectComponent label="Units:"  placeholder="units" multiple={false} options={unitsValue} onChange={() => { }}/>
-      <Input label="Image:" type="url" placeholder="image url" onChange={() => { }} />
-      <Input label="Time:" type="number" placeholder="time in minutes" onChange={() => { }} />
-      <Input label="Amount:" type="number" placeholder="amount" onChange={() => { }} />
-      <Input label="Servings Count:" type="number" placeholder="number of servings count" onChange={() => { }} />
-      <SelectComponent label="Difficulty:" placeholder="---" multiple={false}
-        options={difficultyValue} onChange={() => { }}
-      />
-      <SelectComponent label="Categories:" placeholder="---" multiple={ true}
-        options={categoriesValue} onChange={handleMultiple}
-      />
-
+    <div className="recipeFormContainer">
+      <div className="formsContainer">
+        <Input label="Title :" type="text" placeholder="Title" onChange={() => { }} />
+        <Input label="Title UA:" type="text" placeholder="Назва" onChange={() => { }} />
+        <Input label="Description:" type="text" placeholder="Description" onChange={() => { }} />
+        <Input label="Description UA:" type="text" placeholder="опис" onChange={() => { }} />
+        <SelectComponent label="Units:"
+          placeholder="units" multiple={false} options={unitsValue} onChange={() => { }}
+        />
+        <Input label="Image:" type="url" placeholder="image url" onChange={() => { }} />
+        <Input label="Time:" type="number" placeholder="time in minutes" onChange={() => { }} />
+        <Input label="Amount:" type="number" placeholder="amount" onChange={() => { }} />
+        <Input label="Servings Count:" type="number" placeholder="number of servings count" onChange={() => { }} />
+        <SelectComponent label="Difficulty:" placeholder="---" multiple={false}
+          options={difficultyValue} onChange={() => { }}
+        />
+        <SelectComponent label="Categories:" placeholder="---" multiple={ true}
+          options={categoriesValue} onChange={handleMultiple}
+        />
+      </div>
       <div className="dynamicFormContainer">
         <div  className="dynamicForm">
           {ingredientsFormArray.map((index) => {
@@ -125,7 +128,6 @@ export function RecipeForm(): JSX.Element {
           <Button title="+" onClick={onAddInstructionForm} />
         </div>
       </div>
-
       <Button title="Submit" onClick={onSend} />
     </div>
   );
