dca423739f387304bcc09859c69979eb3ed83776
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-10-25 19:19:47 +0300

    Change Instructions title

--- src/screens/RecipeDetails/components/InstructionsList/styles.ts
+++ src/screens/RecipeDetails/components/InstructionsList/styles.ts
@@ -9,4 +9,15 @@ export const styles = StyleSheet.create({
     padding: 20,
     backgroundColor: Colors.background,
   },
+  header: {
+    flexDirection: 'row',
+    width: '100%',
+    marginBottom: 10,
+    alignItems: 'center',
+    paddingHorizontal: 5,
+  },
+  title: {
+    fontSize: 20,
+    flex: 1,
+  },
 });
