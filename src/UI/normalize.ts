c5539fb2086d0713d988f24d1b02e2d323f2252f
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-11-07 14:22:48 +0200

    Create unit tests for normalize function.Remove conditions for Platform in normalize function

--- src/UI/normalize.ts
+++ src/UI/normalize.ts
@@ -1,19 +1,13 @@
-import { Dimensions, PixelRatio, Platform } from 'react-native';
-
-const {
-  width: SCREEN_WIDTH,
-} = Dimensions.get('window');
-
-const ANDROID_NORMALIZE = 2;
-
-const scale = SCREEN_WIDTH / 320;
+import { Dimensions, PixelRatio } from 'react-native';
 
 export function normalize(size: number) {
-  const newSize = size * scale;
-  if (Platform.OS === 'ios') {
+  const {
+    width: SCREEN_WIDTH,
+  } = Dimensions.get('window');
 
-    return Math.round(PixelRatio.roundToNearestPixel(newSize));
-  } else {
-    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - ANDROID_NORMALIZE;
-  }
+  const scale = SCREEN_WIDTH / 320;
+
+  const newSize = size * scale;
+
+  return Math.round(PixelRatio.roundToNearestPixel(newSize));
 }
