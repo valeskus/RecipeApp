a2dd85de49be72dab95f23fc30def0810b10378a
Valentyna_Olkhovska <valeskus13@gmail.com>
2023-11-26 23:49:51 +0200

    Chenge ingredient item dot style

--- src/screens/RecipeDetails/components/IngredientsListItem/IngredientsListItem.tsx
+++ src/screens/RecipeDetails/components/IngredientsListItem/IngredientsListItem.tsx
@@ -21,18 +21,17 @@ export function IngredientsListItem({
   return (
     <View style={styles.ingredientsListItemContainer}>
       <Image source={Icons.dot} style={styles.ingredientsListItemDot} />
-      <View style={styles.ingredientsListItemContentContainer}>
-        <View style={styles.ingredientsListItemContent}>
-          <Text style={styles.ingredientsListItemTitle}>{title}</Text>
-          {description && (
-            <Text style={styles.ingredientsListItemDescription}>
-              {description}
-            </Text>
-          )}
-        </View>
-        <Text style={styles.ingredientsListItemAmount}>{amount} </Text>
-        <Text style={styles.ingredientsListItemAmount}>{units}</Text>
+
+      <View style={styles.ingredientsListItemContent}>
+        <Text style={styles.ingredientsListItemTitle}>{title}</Text>
+        {description && (
+          <Text style={styles.ingredientsListItemDescription}>
+            {description}
+          </Text>
+        )}
       </View>
+      <Text style={styles.ingredientsListItemAmount}>{amount} </Text>
+      <Text style={styles.ingredientsListItemAmount}>{units}</Text>
     </View>
   );
 }
--- src/screens/RecipeDetails/components/IngredientsListItem/styles.ts
+++ src/screens/RecipeDetails/components/IngredientsListItem/styles.ts
@@ -4,21 +4,20 @@ import { Colors } from '@UI/Colors';
 
 export const styles = StyleSheet.create({
   ingredientsListItemContainer: {
-    width: '100%',
-    flexDirection: 'row',
-    marginBottom: 15,
-  },
-  ingredientsListItemContentContainer: {
     flex: 1,
     flexDirection: 'row',
     borderBottomColor: Colors.borderTextSecondary,
     borderBottomWidth: 1,
     paddingBottom: 15,
+    marginBottom: 15,
+    alignItems: 'baseline',
   },
+
   ingredientsListItemContent: {
     flexDirection: 'column',
     flex: 1,
   },
+
   ingredientsListItemTitle: {
     color: Colors.text,
     fontSize: 18,
@@ -26,20 +25,23 @@ export const styles = StyleSheet.create({
     alignSelf: 'flex-start',
     marginRight: 5,
   },
+
   ingredientsListItemDescription: {
     color: Colors.borderTextSecondary,
     fontSize: 13,
     fontFamily: 'Montserrat-Regular',
   },
+
   ingredientsListItemAmount: {
     color: Colors.text,
     fontSize: 18,
     fontFamily: 'Montserrat-Regular',
   },
+
   ingredientsListItemDot: {
     width: 20,
     height: 20,
-    alignSelf: 'flex-start',
     marginRight: 5,
+    top: 2,
   },
 });
