92162b2f9970843a0c110f6dd5a1e789c1a887df
Valentyna_Olkhovska <valeskus13@gmail.com>
2024-04-05 16:19:01 +0300

    Change recipes action.Formate code

--- src/stores/recipes/recipesActions.ts
+++ src/stores/recipes/recipesActions.ts
@@ -23,8 +23,8 @@ const actionFilterUpdate = (payload: RecipeListModel) => ({
   payload,
 });
 
-const actionSetCardType = (cardType:  'grid' | 'linear') => ({
-  type: RecipesActions.GET,
+const actionSetCardType = (cardType: 'grid' | 'linear') => ({
+  type: RecipesActions.SET_CARD_TYPE,
   payload: {
     cardType,
   },
@@ -79,12 +79,12 @@ export const filterUpdate = async (
 };
 
 export const setCardType = (
-  cardType:  'grid' | 'linear',
+  cardType: 'grid' | 'linear',
   dispatch: Dispatch,
 ) => {
   try {
     dispatch(actionSetCardType(cardType));
-  } catch (error){
+  } catch (error) {
     dispatch(actionError('setCardType', error));
   }
 };
