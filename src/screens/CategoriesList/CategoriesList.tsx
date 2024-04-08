d89c31bb0ec2078d4e78095dce97040af9454bdc
Valentyna_Olkhovska <valeskus13@gmail.com>
2024-04-08 13:49:30 +0300

    change categories list  screen

--- src/screens/CategoriesList/CategoriesList.tsx
+++ src/screens/CategoriesList/CategoriesList.tsx
@@ -19,7 +19,7 @@ export function CategoriesList(): JSX.Element {
       </View>
       {isError && <Error onRetry={fetchData} />}
       {isLoading && <CategoryListSkeleton />}
-      {!isLoading && <CategoryCards categories={categories} />}
+      {!isLoading && !isError && <CategoryCards categories={categories} />}
     </View>
   );
 }
