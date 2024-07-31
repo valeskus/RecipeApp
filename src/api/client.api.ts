577d494e0065a36bafedc6ee9c75787bcba72f28
Valentyna_Olkhovska <valeskus13@gmail.com>
2024-07-31 17:37:43 +0300

    Create axios interceptors with error handler

--- src/api/client.api.ts
+++ src/api/client.api.ts
@@ -2,11 +2,25 @@ import axios from 'axios';
 
 import { RemoteConfigManager } from '@managers/RemoteConfigManager';
 
+import { EventService } from '@services/EventService';
+
 export const client = axios.create({});
 
 export function init() {
   client.defaults.baseURL = RemoteConfigManager.get('API_URL');
 }
+
+client.interceptors.response.use((response) =>
+  response,
+  (error) => {
+    EventService
+      .emit('app:error', {
+        moduleName: `${error.config.url},${error.config.method},${error.message}`,
+        error: new Error(error.url),
+      });
+
+    return error;
+  });
 
 export const changeLanguage = (language: string) => {
   if (!language) {
