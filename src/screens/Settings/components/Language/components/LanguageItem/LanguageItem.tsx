885efb6584c9b514a81fe09d3ebdb815969078c8
Valentyna_Olkhovska <valeskus13@gmail.com>
2024-09-01 13:51:00 +0300

    REmove redundant code

--- src/screens/Settings/components/Language/components/LanguageItem/LanguageItem.tsx
+++ src/screens/Settings/components/Language/components/LanguageItem/LanguageItem.tsx
@@ -7,7 +7,7 @@ import { styles } from './styles';
 
 interface Props {
   languageCode: 'en' | 'ua';
-  language: 'en' | 'ua' | undefined;
+  language?: 'en' | 'ua';
   onPress: () => void;
   title: string;
 }
